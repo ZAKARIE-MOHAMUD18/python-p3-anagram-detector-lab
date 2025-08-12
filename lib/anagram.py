f531f32d428d54357637f0ff8e14ee9825fc2c7e
zakarie <zakarie.abdi@student.moringaschool.com>
2025-08-12 15:47:04 +0300

    initial commit

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,23 @@
 # your code goes here!
+
+class Anagram:
+    def  __init__(self,word):
+        self.word = word.lower()
+        
+    
+    def match(self,candidates):
+        matches=[]
+        sorted_word = sorted(self.word)
+
+        for candidate in candidates:
+            if candidate.lower() == self.word:
+                continue
+            if sorted(candidate.lower()) == sorted_word:
+                matches.append(candidate)
+
+        return matches
+            
+
+listen = Anagram("listen")
+listen.match(['enlists', 'google', 'inlets', 'banana'])
+
